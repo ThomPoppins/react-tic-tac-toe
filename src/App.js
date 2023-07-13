b5395a13ffa0cc2984eb27b898f2eb3bd851b2e9
Thom Poppins <thompoppins@gmail.com>
2023-07-13 02:30:35 +0100

    button value changes to X when clicked using useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React, { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return <button
+    className="square"
+    onClick={handleClick}
+  >
+    {value}
+  </button>;
+}
+
+export default function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  )
 }
